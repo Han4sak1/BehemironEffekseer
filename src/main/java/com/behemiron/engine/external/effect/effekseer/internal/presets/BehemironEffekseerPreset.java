d2a88ad5b6271ad39715df7ae8ee10712fc5c8e3
Gei <none@none>
2026-06-14 16:54:55 +0800

    fix(maven): attach platform classifier artifacts

--- src/main/java/com/behemiron/engine/external/effect/effekseer/internal/presets/BehemironEffekseerPreset.java
+++ src/main/java/com/behemiron/engine/external/effect/effekseer/internal/presets/BehemironEffekseerPreset.java
@@ -3,11 +3,13 @@ package com.behemiron.engine.external.effect.effekseer.internal.presets;
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.annotation.Platform;
 import org.bytedeco.javacpp.annotation.Properties;
+import org.bytedeco.javacpp.presets.javacpp;
 
 /**
  * JavaCPP preset 定义，负责声明 C API 头文件和 Native 库链接关系。
  */
 @Properties(
+        inherit = javacpp.class,
         value = {
                 @Platform(
                         include = "BehemironEffekseerCAPI.h",
